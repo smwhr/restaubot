4b4f7e9865407d602b89b3630dac70b6a356c65b
Ju/TroisYaourts <julien@troisyaourts.com>
2016-11-04 12:54:05 +0100

    send message back

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 require_once(__DIR__."/../app/bootstrap.php");
 
@@ -15,7 +16,36 @@ $app->before(function(Request $request){
 });
 
 $app->match('/webhook-malin', function(Request $request) use ($app){
-  return new Response("ok");
+  $message = $request->get("message",[]);
+  if(empty($message)) return new Response("Empty message");
+
+
+  $message_id = $message['message_id'];
+  $chat_id = $message['chat']['id'];
+
+
+  if (isset($message['text'])) {
+    // incoming text message
+    $text = $message['text'];
+
+    if (strpos($text, "/start") === 0) {
+      $app["messager"]("sendMessage", array('chat_id' => $chat_id, "text" => 'Hello', 'reply_markup' => array(
+        'keyboard' => array(array('Hello', 'Hi')),
+        'one_time_keyboard' => true,
+        'resize_keyboard' => true)));
+    } else if ($text === "Hello" || $text === "Hi") {
+      $app["messager"]("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'), true);
+    } else if (strpos($text, "/stop") === 0) {
+      // stop now
+    } else {
+      return new JsonResponse(["method" => "sendMessage",
+                               "chat_id" => $chat_id, 
+                               "reply_to_message_id" => $message_id, 
+                               "text" => "Cool")
+    }
+  } else {
+    $app["messager"]("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'), true);
+  }
 });
 
 
